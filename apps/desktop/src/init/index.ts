16ce350b07c4c929dd2e961abc97511486597f71
lincoln <1043156036@qq.com>
2024-11-09 20:15:13 +0800

    feat: devtools no focus & correct position

--- apps/desktop/src/init/index.ts
+++ apps/desktop/src/init/index.ts
@@ -1,10 +1,14 @@
 import { installChromeExtensions } from "@lib/electron-utils";
 import { isDevelopment, isTest } from "@src/utils";
+import { nativeTheme } from "electron";
 
 export async function initElectron() {
+  console.log(`[init] init electron...`);
   if ([isDevelopment, isTest].some(Boolean)) {
     await installChromeExtensions(["REACT_DEVELOPER_TOOLS"]);
   }
+
+  nativeTheme.themeSource = "system";
 }
 
 export async function initService() {
--- apps/desktop/src/main.ts
+++ apps/desktop/src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, screen } from "electron";
+import { app, BrowserWindow, Rectangle, screen } from "electron";
 import path from "node:path";
 import url from "node:url";
 import {
@@ -51,8 +51,8 @@ async function startupWindow() {
   const displays = screen.getAllDisplays();
   const targetDisplay = displays[displays.length > 1 ? 1 : 0];
 
-  const { width, height } = { width: 800, height: 600 };
-  const x = targetDisplay.bounds.x + (targetDisplay.bounds.width - width) / 2;
+  const { width, height } = { width: 1200, height: 900 };
+  const x = targetDisplay.bounds.x + (targetDisplay.bounds.width - width) * 0.3;
   const y = targetDisplay.bounds.y + (targetDisplay.bounds.height - height) / 2;
 
   const options: Electron.BrowserWindowConstructorOptions = {
@@ -71,6 +71,7 @@ async function startupWindow() {
       sandbox: false,
       webSecurity: true,
       // webSecurity: !isDev,
+      // devTools: isDev || isTest,
       spellcheck: false,
     },
   };
@@ -98,12 +99,61 @@ async function startupWindow() {
   }
 
   if (NO_FOCUS) {
+    const windowShowPromise = new Promise<void>((resolve) =>
+      mainWindow.once("show", () => resolve()),
+    );
     // no focus when reboot app from dev:main
     mainWindow.showInactive();
+    await windowShowPromise;
   } else {
     mainWindow.show();
   }
 
-  console.log(`window started`);
+  initDevToolsWindow(mainWindow);
+  mainWindow.webContents.openDevTools({ mode: "detach" });
+
+  console.log(`[main] window started`);
   return mainWindow;
+}
+
+/**
+ * use custom browserWindow for devTools,
+ * so that u can update its bounds and
+ * use showInactive to prevent focus devToolsWindow when it opened trigger by reboot
+ */
+function initDevToolsWindow(hostWindow: BrowserWindow) {
+  const hostBounds = hostWindow.getBounds();
+  const hostDisplay = screen.getDisplayNearestPoint({
+    x: hostBounds.x,
+    y: hostBounds.y,
+  });
+  // right placement of host window
+  const devBounds: Rectangle = { x: 0, y: 0, width: 800, height: 1000 };
+  devBounds.x +=
+    hostBounds.x +
+    hostBounds.width +
+    (hostDisplay.bounds.x +
+      hostDisplay.bounds.width -
+      hostBounds.x -
+      hostBounds.width -
+      devBounds.width) /
+      2;
+  devBounds.y +=
+    hostDisplay.bounds.y + (hostDisplay.bounds.height - devBounds.height) / 2;
+
+  const devToolsWindow = new BrowserWindow({
+    show: false,
+    ...devBounds,
+  });
+  hostWindow.webContents.setDevToolsWebContents(devToolsWindow.webContents);
+  hostWindow.webContents.on("devtools-opened", async () => {
+    // show win
+    if (NO_FOCUS) {
+      devToolsWindow.showInactive();
+    } else {
+      devToolsWindow.show();
+    }
+  });
+
+  return devToolsWindow;
 }
